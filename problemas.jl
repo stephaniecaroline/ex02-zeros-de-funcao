30fa5eccf441e7f4cc36fcdb2eb0fecd070dfb7d
stephaniecaroline <54449173+stephaniecaroline@users.noreply.github.com>
2019-10-04 13:52:52 -0300

    Update problemas.jl

--- problemas.jl
+++ problemas.jl
@@ -10,4 +10,14 @@ const ProblemList =
   EquacaoNL(x -> x^5 - 2^5, 1.0, "Grau 5"),
   EquacaoNL(x -> sin(x) + max(3 - x, 0)^2 + max(x - 3.2, 0)^2, 1.0, "π por sin"),
   EquacaoNL(x -> 1 / (1 + exp(-0.1x + 1)) - 0.5, 0.0, "Sigmoid"),
+  EquacaoNL(x -> tan(exp(x^6)+1)/exp(x^2), 1.0, "Tangente e exp"),
+  EquacaoNL(x -> sin(exp(x)) * log(x^2+1), 3.0, "Função tipo seno de log e exp"),
+  EquacaoNL(x -> tan(exp(x^2+2x)), 1.0, "Exp e Tangente"),
+  EquacaoNL(x -> ln(x^2 + 8)*x/ exp(x), -1.0, "Secante"),
+  EquacaoNL(x -> (exp(x^2) - exp(x+4))/exp(x)+5, -1.6, "Exp"),
+  EquacaoNL(x -> (x/7) - 2x^2 + 15x^3 - 4x^4 +12, 1.0, "Quadrica"),
+  EquacaoNL(x -> cos(x) - (x/7) + 18, 1.0, "Função tipo cosseno"),
+  EquacaoNL(x -> x*cos(3*exp(x)) + 8, 1.0, "Função tipo cosseno e exp"),
+  EquacaoNL(x -> sin(x^3 + 2x - 3), 0.0, "Função tipo seno"),
+  EquacaoNL(x -> cos(x-3) - exp(x^2) + 5, 0.0, "Função tipo cosseno e exp")
 ]
