f813a6858ed646a1ea92486b17cd67889b3e6160
stephaniecaroline <54449173+stephaniecaroline@users.noreply.github.com>
2019-10-07 19:04:53 -0300

    Update problemas.jl

--- problemas.jl
+++ problemas.jl
@@ -15,5 +15,9 @@ const ProblemList =
   EquacaoNL(x -> x*cos(3*exp(x)) + 8, 1.0, "Função tipo cosseno e exp"),
   EquacaoNL(x -> sin(x^3 + 2x - 3), 0.0, "Função tipo seno"),
   EquacaoNL(x -> exp((x^2)/10000) - ((exp(x+4))/(exp(x)+5)) ,-2.0, "Função exp"),
-  EquacaoNL(x -> cos(x-3) - exp((x^2)/10000) + 1, 1.0, "Função tipo cosseno e exp")
+  EquacaoNL(x -> cos(x-3) - exp((x^2)/10000) + 1, 1.0, "Função tipo cosseno e exp"),
+  EquacaoNL(x -> ln(x^2 + 8)*x/exp(sqrt(x^2+1), -1.0, "Função log e exp"),
+  EquacaoNL(x -> sen(x^2 + pi) * x/10, 1.0, "Função seno"),
+  EquacaoNL(x -> arcos(3*x + 4) * 5*sen(x), 1.0, "Função arcoseno e seno"),
+  EquacaoNL(x -> 3 *  log(x^2 +6)*cos(x)/x^2 +8, 0.0, "Função log e cos")
 ]
